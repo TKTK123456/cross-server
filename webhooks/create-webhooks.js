38545a473825f7e1d2f4fa768a82b7720e73443e
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2024-07-16 23:42:01 +0000

    update

--- webhooks/create-webhooks.js
+++ webhooks/create-webhooks.js
@@ -1,9 +1,23 @@
-import { Client, GatewayIntentBits, Collection, Events, Activity, SlashCommandBuilder} from 'discord.js';
+import { Client, GatewayIntentBits, Collection, Events, Activity, SlashCommandBuilder, Webhook, ChannelType} from 'discord.js';
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers] });
-
 console.log('Runing create webhooks worked!')
-
-
+let webhook;
+export default async function getWebhooks() {
+  console.log('getting webhooks')
+  const webhooks = [];
+  for (const guilds of client.guilds.cache) {
+    const channels = guilds.filter(channel => channel.type === ChannelType.GuildText);
+    for (const channel of channels.values()) {
+      const fetchedWebhooks = await channel.fetchWebhooks();
+      webhooks.push(...fetchedWebhooks);
+    }
+  }
+  webhook = webhooks
+  return webhooks;
+}
+client.on('ready', () => {
+  getWebhooks()
+})
 
 
 
